14ca22ab5fddffb09737b3cbf7b66e9cd3d105cc
Delwser <87214159+Delwser@users.noreply.github.com>
2026-02-25 22:31:16 -0300

    Add files via upload

---
+++ kali-apt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#instalar o kali-apt
+
+
+# adicionando o  source.list do Kali Linux.
+echo "# Kali Repo:" | sudo tee -a /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
+
+# instalando a chave gpg para uso do apt.
+
+sudo apt install wget
+sleep 5
+wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2025.1_all.deb
+sleep 5
+sudo dpkg -i kali-archive-keyring_2025.1_all.deb
+sleep 3
+rm kali-archive-keyring_2025.1_all.deb
+echo "Finalizado - Tente um apt update."
