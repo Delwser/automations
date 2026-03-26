3fb9439fbb8428f676e79c34080b3ca3235ba2f1
Delwser <87214159+Delwser@users.noreply.github.com>
2026-03-26 14:56:10 -0300

    Update connect.sh
    Add a new feature: the script can connect to the Hacking Club VPN.

--- connect.sh
+++ connect.sh
@@ -4,15 +4,18 @@
 
 #HTB -- Hack The Box
 #THM -- Try Hackme
+#HKC -- Hacking Club
 
-HTBPATH='' #Coloque aqui o path do arquivo .ovpn do Hack The Box.
+HTBPATH='' # Coloque aqui o path do arquivo .ovpn do Hack The Box.
 THMPATH='' # Coloque aqui o path do arquivo .ovpn do TryHackme.
+HKCPATH='' # Coloque aqui o path do arquivo .ovpn do Hacking Club.
 
 if [ -z "$1" ]; then
 	echo "Precisa passar um parametro"
 	echo "Exemplo:"
 	echo "./connect THM -- (Conectar a VPN do Tryhackme)"
 	echo "./connect HTB -- (Conectar a VPN do HackTheBox)"
+	echo "./connect HKC -- (Conectar a VPN do HackTheBox)"
 fi
 
 
@@ -27,6 +30,11 @@ HTB() {
 
 }
 
+HKC() {
+        echo "connecting Hacking Club VPN..."
+        sudo openvpn $HKCPATH
+
+}
 
 if [ "$1" = "THM" ]; then
 	echo "Opcao selecionada" $1
@@ -36,4 +44,9 @@ fi
 if [ "$1" = "HTB" ]; then
 	echo "Opcao selecionada" $1
 	HTB
+fi
+
+if [ "$1" = "HKC" ]; then
+	echo "Opcao selecionada" $1
+	HKC
 fi
