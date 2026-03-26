94bdcaf0c64db670dc3ddb64ec2d4ef32d485afc
Delwser <87214159+Delwser@users.noreply.github.com>
2026-03-26 12:56:00 -0300

    Update kali-apt.sh
    fix na url - pessoal do kali fez update na keyring

--- kali-apt.sh
+++ kali-apt.sh
@@ -11,9 +11,9 @@ echo "deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-
 
 sudo apt install wget
 sleep 5
-wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2025.1_all.deb
+wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2025.2_all.deb
 sleep 5
-sudo dpkg -i kali-archive-keyring_2025.1_all.deb
+sudo dpkg -i kali-archive-keyring_2025.2_all.deb
 sleep 3
 rm kali-archive-keyring_2025.1_all.deb
 echo "Finalizado - Tente um apt update."
